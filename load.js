9430a8a20a4ee6087fc7e36c861990e0c24f1227
Callum Bryson <callumjbryson@gmail.com>
2016-05-21 16:28:16 +1000

    Fixed broken links

--- load.js
+++ load.js
@@ -1,6 +1,21 @@
 $(document).ready( function () {
     //list of pages
-    var pageIDs = ["gnaV1S", "jYEgug", "eRfgZB"]
+    var pageIDs = ["gnaV1S",
+                   "jYEgug",
+                   "eRfgZB",
+                   "lClRdj",
+                   "1B7Atg",
+                   "pBoFAe",
+                   "z7muXR",
+                   "rmabJF",
+                   "4g9gYa",
+                   "xPf8xo",
+                   "NZttk4",
+                   "y8kGQT",
+                   "sfY6pe",
+                   "mFPv0o",
+                   "VnoC0j"
+                  ]
     //look for when the form changes
     var id = "";
     var text = "";
@@ -11,7 +26,8 @@ $(document).ready( function () {
             text= "Enter the ID of a page to continue the story."
         } else if (!(pageIDs.indexOf(id) === -1)) {
             num = pageIDs.indexOf(id) + 1;
-            $("#loadResult").html("<a href = '" + num + ".htm'>Page " + num + "</a>");
+            $("#loadResult").html("<a href = '" + num + ".htm'>Page " + num + "</a>" +
+                                 "<p><img src='images/" + num + ".png' />");
         }
     });
 });
